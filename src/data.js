04dee7c12b923bc74bfea4e9fe47f1403b7b0e77
markkennedy7500 <markkennedy757@gmail.com>
2023-07-10 10:00:16 -0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "Images/summer_sale.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "Images/winter.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "Images/slidder.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "Images/pexels_photo.jpeg",
+    title: "SHIRT STYLE!",
+    cat: "women",
+  },
+  {
+    id: 2,
+    img: "Images/pexels_photo2.jpeg",
+    title: "LOUNGEWEAR LOVE",
+    cat: "coat",
+  },
+  {
+    id: 3,
+    img: "Images/pexels_photo3.jpeg",
+    title: "LIGHT JACKETS",
+    cat: "jeans",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "Images/popular.png",
+  },
+  {
+    id: 2,
+    img: "Images/popularproduct2.png",
+  },
+  {
+    id: 3,
+    img: "Images/popularproduct3.png",
+  },
+  {
+    id: 4,
+    img: "Images/popularproucts.png",
+  },
+  {
+    id: 5,
+    img: "Images/Mens-jake-Guitar-Vintage-1.png",
+  },
+  {
+    id: 6,
+    img: "Images/mens wear.jpg",
+  },
+  {
+    id: 7,
+    img: "Images/Mens-jake-Guitar-Vintage-1.png",
+  },
+  {
+    id: 8,
+    img: "Images/popularproduct3.png",
+  },
+];
---
+++ src/pages/Success.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
+
+const Success = () => {
+  const location = useLocation();
+  return (
+    <div>
+     successful
+    </div>
+);
+};
+
+export default Success;
